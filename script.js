1f67a21d1b71f9f04b23824194eed394e2cd3af8
Ryan Cahela <ryancahela@gmail.com>
2019-02-09 14:40:19 -0800

    finished first draft working app

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ const questionBank = [
             getCorrect: function() {
                 return this.answer3;
             },
-            correctDetail: "Others contend that pierogi were brought to Poland by Saint Hyacinth of Poland, who brought them back from Kiev (the center of Kievan Rus', nowadays the capital of Ukraine)."
+            correctDetail: `Others contend that pierogi were brought to Poland by Saint Hyacinth of Poland, 
+            who brought them back from Kiev (the center of Kievan Rus', nowadays the capital of Ukraine).`
         },
         imageUrl: "img/perogi2.jpg",
         imageAlt: "white, round plate with seven perogi with light browning."
@@ -33,11 +34,149 @@ const questionBank = [
         },
         imageUrl: "img/cheeseburger1.jpg",
         imageAlt: "cheeseburger with a sesame seed bun, lettuce, tomato, and onion."
+      },
+      {
+        question: "Who invented Tofu?",
+        answers: {
+            answer1: "China",
+            answer2: "Italy",
+            answer3: "1960's hippies",
+            answer4: "Iceland",
+            getCorrect:  function() {
+                return this.answer1;
+            },
+            correctDetail: "Tofu has been consumed for more than 2000 years in China"
+        },
+        imageUrl: "img/tofu2.jpg",
+        imageAlt: `a block of solid tofu on a wooden cutting boar. A small pile of greens and a small pile 
+        of cubed tofu are also on the cutting board.`
+      },
+      {
+        question: "Where was the first pizza made according to pizza lore?",
+        answers: {
+            answer1: "Italy",
+            answer2: "New York, USA",
+            answer3: "Greece",
+            answer4: "Japan",
+            getCorrect:  function() {
+                return this.answer1;
+            },
+            correctDetail: `An often recounted story holds that on 11 June 1889, to honour the Queen consort 
+            of Italy, Margherita of Savoy, the Neapolitan pizza maker Raffaele Esposito created the "Pizza Margherita"
+            , a pizza garnished with tomatoes, mozzarella, and basil, to represent the national colours of Italy as 
+            on the Italian flag.`
+        },
+        imageUrl: "img/pizza1.jpg",
+        imageAlt: "A large New York style cheese pizza on a wooden cutting board, one slice removed."
+      },
+      {
+        question: "Who started making the donuts in their round with a hole form?",
+        answers: {
+            answer1: "United States",
+            answer2: "Germany",
+            answer3: "England",
+            answer4: "Iran",
+            getCorrect:  function() {
+                return this.answer1;
+            },
+            correctDetail: `Hanson Gregory, an American, claimed to have invented the ring-shaped doughnut 
+            in 1847 aboard a lime-trading ship when he was 16 years old. Gregory was dissatisfied with the 
+            greasiness of doughnuts twisted into various shapes and with the raw center of regular doughnuts.`
+        },
+        imageUrl: "img/donuts2.jpg",
+        imageAlt: "A variety of donuts on a wire cooling rack. Some have glazes or frosting, while others have crunchy bits."
+      },
+      {
+        question: "Cupcakes are not just for elementary school-but where did they first appear?",
+        answers: {
+            answer1: "United States",
+            answer2: "England",
+            answer3: "Finland",
+            answer4: "Scotland",
+            getCorrect:  function() {
+                return this.answer1;
+            },
+            correctDetail: `The earliest extant description of what is now often called a cupcake was in 1796,
+             when a recipe for "a light cake to bake in small cups" was written in American Cookery by Amelia Simmons`
+        },
+        imageUrl: "img/cupcake1.jpg",
+        imageAlt: "a yellow cupcake cunt in half on a plate. The frosting is white with tinges of blue and spilling from the center are sprinkles and candy pieces"
+      },
+      {
+        question: "Curry is a favorite in many countries but what country started the trend?",
+        answers: {
+            answer1: "India",
+            answer2: "England",
+            answer3: "Russia",
+            answer4: "Canada",
+            getCorrect:  function() {
+                return this.answer1;
+            },
+            correctDetail: `Curry (plural curries) is a variety of dishes originating in the Indian subcontinent 
+            that use a complex combination of spices or herbs, usually including ground turmeric, cumin, coriander,
+             ginger, and fresh or dried chilies.`
+            },
+        imageUrl: "img/curry3.jpg",
+        imageAlt: "Red curried chicken with green vegtable in a ceramic, two handled pot."
+      },
+      {
+        question: "Nothing beats Scotch Eggs as bar food but who came up with it?",
+        answers: {
+            answer1: "England",
+            answer2: "Ireland",
+            answer3: "Scotland",
+            answer4: "Canada",
+            getCorrect:  function() {
+                return this.answer1;
+            },
+            correctDetail: `The London department store Fortnum & Mason claims to have invented Scotch eggs in 
+            1851 but the name for the snack was supposedly derived from a nickname used by Londoners who lived 
+            around Wellington Barracks after Officers of the Scots Guards stationed there developed a taste for the snack`
+        },
+        imageUrl: "img/scotcheggs2.jpg",
+        imageAlt: `Three Scotch egg halvs yolk up and one half down on a wooden cutting board with a cup 
+        of mustard. Scotch eggs are medium hard eggs wrapped in sausage, rolled in breadcrumbs and fried.`
+      },
+      {
+        question: "Hamburgers might surprise you-who enjoyed the first?",
+        answers: {
+            answer1: "United States",
+            answer2: "Germany",
+            answer3: "Scotland",
+            answer4: "Sweden",
+            getCorrect:  function() {
+                return this.answer1;
+            },
+            correctDetail: `The term hamburger originally derives from Hamburg,[1] Germany's s
+            econd-largest city but the actual sandwich may have been done first in New Haven CT, 
+            in 1900 Louis Lassen, a Danish immigrant, owner of Louis' Lunch.`
+        },
+        imageUrl: "img/hamburger2.jpg",
+        imageAlt: "grilled beef patty on e sesame seed bun with ketchup and pickles."
+      },
+      {
+        question: "Mix your soy and Wasabi-who brought us Sushi?",
+        answers: {
+            answer1: "South East Asia",
+            answer2: "India",
+            answer3: "South America",
+            answer4: "Norway",
+            getCorrect:  function() {
+                return this.answer1;
+            },
+            correctDetail: `The earliest form of sushi, a dish today known as narezushi, 
+            has its probable origin with paddy fields along the Mekong river in Southeast Asia. 
+            The prototypical narezushi is made by lacto-fermenting fish with salt and rice in
+             order to control putrefaction.( Southeast Asia)`
+        },
+        imageUrl: "img/sushi3.jpg",
+        imageAlt: "An oval platter with sliced nori wrapped sushi rolls stacked.Chopsticks lay across the platter."
       }
 
 ];
 
-let score;
+
+let score=0;
 let answerIsCorrect = true;
 let appState = "start-view";
 let resultMessage;
@@ -127,6 +266,19 @@ function buildResultView(STORE) {
 
 }
 
+function buildEndView() {
+    return $(`<section id="end-view" class="">
+    <header role="banner">
+        <h1>Push Food Origin Master</h1>
+    </header>
+    <main role="main">
+        <h3>${score}</h3>
+        <img src="" alt="">
+        <button class="js-retake-quiz">Retake Quiz</button>
+    </main>
+    </section>`);   
+}
+
 // As a user, I should be able to view the questions and answers.
 // This function will render the quiz question with answers to select
 function renderView(appState){
@@ -148,6 +300,12 @@ function renderView(appState){
         $('.body').html(finishedHtml);
     }
 
+    if(appState === "end-view") {
+        let finishedHtml = buildEndView(STORE);
+        $('.body').html(finishedHtml);
+    }
+
+  console.log(`currentQustion${currentQuestion}`);  
   console.log('`renderView` ran');
     
 }
@@ -193,6 +351,8 @@ function showAnswerResult(){
   appState = "results-view";
   renderView(appState);
   moveToNextQuestion();
+  currentQuestion++
+  console.log(`score:${score}`);
 }
 
 // As a user, I should be able to move to the next question.
@@ -201,25 +361,42 @@ function showAnswerResult(){
 function moveToNextQuestion(){
      //detect click on next question button
     $('.next-question-btn').on('click', function(event) {
-        currentQuestion++
-        STORE.pop();
-        STORE.push(questionBank[currentQuestion]);
-        console.log(STORE);
-        appState = "question-view";
-        renderView(appState);
+        if(currentQuestion <= 9) {
+            STORE.pop();
+            STORE.push(questionBank[currentQuestion]);
+            appState = "question-view";
+            renderView(appState);
+        }
+
+        if(currentQuestion > 9) {
+            appState = "end-view";
+            showFinalScore(); 
+        }
+
+    
     });
   console.log('`moveToNextQuestion` ran');
 }
 
 // As a user, I should be able to view my score at the end.
 // this function displays the final score with a restart button to play again
-function renderFinalScore(){
-  console.log('`renderFinalScore` ran');
+function showFinalScore(){
+    renderView(appState);
+    replayQuiz();
+    console.log('`showFinalScore` ran');
 }
 
 // As a user, I should have the option of restarting.
 // this function listens for the quiz restart button and restarts if needed 
 function replayQuiz(){
+    $('.js-retake-quiz').on('click',function(event){
+        score = 0;
+        currentQuestion = 0;
+        appState = "start-view";
+        STORE.pop();
+        STORE.push(questionBank[currentQuestion]);
+        startQuiz();
+    });
   console.log('`replayQuiz` ran');
 }
 
